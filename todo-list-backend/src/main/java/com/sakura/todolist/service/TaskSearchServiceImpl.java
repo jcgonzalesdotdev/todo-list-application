e159fc4765e73b37a79874275241a3f7ba03a259
jcgonzalesdotdev <jc.gonzales@ibm.com>
2024-03-11 18:07:49 +0800

    PG/UT: Created JUnit test for search service

--- todo-list-backend/src/main/java/com/sakura/todolist/service/TaskSearchServiceImpl.java
+++ todo-list-backend/src/main/java/com/sakura/todolist/service/TaskSearchServiceImpl.java
@@ -19,6 +19,7 @@ public class TaskSearchServiceImpl implements TaskSearchService {
 	/**
 	 * TaskRepository
 	 */
+	@Autowired
 	private TaskRepository taskRepository;
 
 	@Override
--- todo-list-backend/src/test/java/com/sakura/todolist/TodoListApplicationTests.java
+++ todo-list-backend/src/test/java/com/sakura/todolist/TodoListApplicationTests.java
@@ -2,26 +2,30 @@ package com.sakura.todolist;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sakura.todolist.exceptions.EntityNotFoundException;
 import com.sakura.todolist.model.Task;
 import com.sakura.todolist.repository.TaskRepository;
-import com.sakura.todolist.service.TaskSearchService;
 import com.sakura.todolist.service.TaskSearchServiceImpl;
 
 @SpringBootTest
 class TodoListApplicationTests {
+	
+	@BeforeEach
+	public void setUp() {
+	}
 	
 	@Mock
 	private TaskRepository taskRepository;
@@ -40,12 +44,28 @@ class TodoListApplicationTests {
 		List<Task> mockedListOfTask = new ArrayList<Task>();
 		
 		Task task = new Task();
-		task.setId(Mockito.anyLong());
-		task.setTitle(Mockito.anyString());
+		task.setId((long) 1);
+		task.setTitle("Title 1");
 		mockedListOfTask.add(task);
 		
 		when(taskRepository.findAll()).thenReturn(mockedListOfTask);
 		assertNotEquals(0, taskSearchServiceImpl.getAllTasks().size());
+	}
+	
+	/**
+	 * Name: Test0002
+	 * Condition: Normal input
+	 * Expected result: Result is 0, throws EntityNotFoundException	
+	 */
+	@Test
+	void test002() {
+		
+		List<Task> mockedListOfTask = new ArrayList<Task>();
+		
+		
+		when(taskRepository.findAll()).thenReturn(mockedListOfTask);
+		assertThrows(EntityNotFoundException.class, () -> taskSearchServiceImpl.getAllTasks());
+		
 	}
 
 }
