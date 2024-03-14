0d98a1b3fb2c8440c15d15e12a18f2de712480c1
fureyarin <162277909+fureyarin@users.noreply.github.com>
2024-03-14 13:07:16 +0800

    Delete todo-list-frontend - Copy directory

--- todo-list-frontend - Copy/src/components/landing-page/footer-section.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>Vue.js Landing Page &copy; 2024. All rights reserved.</p>
-    </div>
-  </footer>
-</template>
